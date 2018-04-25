8b968e08d3ca62e19aa3f052ea846f8baecb73e0
ZFT <zhongfateng@126.com>
2018-04-25 10:03:02 +0800

    Merge branch 'master' of https://github.com/realdoctor/RealDoc
    # Conflicts:
    #	app/app.iml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/real/doctor/realdoc/rxjavaretrofit/http/HttpNetUtil.java
+++ app/src/main/java/com/real/doctor/realdoc/rxjavaretrofit/http/HttpNetUtil.java
@@ -8,4 +8,6 @@ public class HttpNetUtil {
     //内容网地址 测试地址  模拟冲突
     //  public static final String BASE_URL = "http://192.168.1.119:8081/";
     public static final String BASE_URL = "http://47.98.156.204:8088/";
+    //模拟冲突测试
 }
+
