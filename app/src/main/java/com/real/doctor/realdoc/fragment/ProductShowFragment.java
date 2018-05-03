9adb0e05f510f83b0cf991aaad1e7ac261a77814
ZFT <zhongfateng@126.com>
2018-05-03 16:07:00 +0800

    Merge branch 'master' of https://github.com/realdoctor/RealDoc
    # Conflicts:
    #	app/app.iml
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/real/doctor/realdoc/fragment/ProductShowFragment.java
+++ app/src/main/java/com/real/doctor/realdoc/fragment/ProductShowFragment.java
@@ -2,22 +2,42 @@ package com.real.doctor.realdoc.fragment;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.real.doctor.realdoc.R;
+import com.real.doctor.realdoc.adapter.BrandAdapter;
+import com.real.doctor.realdoc.adapter.ProductAdapter;
 import com.real.doctor.realdoc.base.BaseFragment;
+import com.real.doctor.realdoc.model.BrandBean;
+import com.real.doctor.realdoc.model.CategoryBean;
+import com.real.doctor.realdoc.model.ProductInfo;
+import com.real.doctor.realdoc.util.ToastUtil;
+import com.real.doctor.realdoc.view.BrandListView;
+
+import butterknife.BindView;
 
 /**
  * Created by Administrator on 2018/4/18.
  */
 
 public class ProductShowFragment extends BaseFragment {
-    public static ProductShowFragment newInstance() {
+    public CategoryBean bean;
+    @BindView(R.id.lv_brands)
+    BrandListView brandListView;
+    @BindView(R.id.lv_products)
+    ListView listView;
+    public BrandAdapter brandAdapter;
+    public ProductAdapter productAdapter;
+    public static ProductShowFragment newInstance(CategoryBean bean) {
+        bean=bean;
         return new ProductShowFragment();
     }
 
     @Override
     public int getLayoutId() {
-        return R.layout.fragment_product_show;
+        return R.layout.fragment_category;
     }
 
     @Override
@@ -26,7 +46,31 @@ public class ProductShowFragment extends BaseFragment {
     }
 
     @Override
-    public void doBusiness(Context mContext) {
+    public void doBusiness(final Context mContext) {
+        brandAdapter =new BrandAdapter(mContext,bean.brands);
+        brandListView.setAdapter(brandAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long arg3) {
+                // TODO Auto-generated method stub
+                final int location=position;
+                brandAdapter.setSelectedPosition(position);
+                brandAdapter.notifyDataSetInvalidated();
+                final BrandBean bean=	(BrandBean) brandAdapter.getItem(position);
+                productAdapter=new ProductAdapter(mContext, bean.productList);
+                listView.setAdapter(productAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> arg0, View arg1,
+                                            int position, long arg3) {
+                        ProductInfo  product= bean.productList.get(position);
+                        ToastUtil.show(mContext,product.getName(), Toast.LENGTH_SHORT);
+                    }
+                });
+
+            }
+        });
 
     }
 
