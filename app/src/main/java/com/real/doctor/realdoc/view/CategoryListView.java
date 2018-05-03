255d88e59e1ed6e6d1944d896a08b913daef0805
ZFT <zhongfateng@126.com>
2018-05-03 15:34:17 +0800

    Merge branch 'master' of https://github.com/realdoctor/RealDoc
    # Conflicts:
    #	app/app.iml
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/real/doctor/realdoc/view/CategoryListView.java
@@ -0,0 +1,24 @@
+package com.real.doctor.realdoc.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ListView;
+
+public class CategoryListView extends ListView  {
+
+    public CategoryListView (Context context, AttributeSet attrs) { 
+          super(context, attrs); 
+    } 
+ 
+    public CategoryListView (Context context, AttributeSet attrs, int defStyle) { 
+          super(context, attrs, defStyle); 
+    } 
+ 
+    public CategoryListView (Context context) { 
+          super(context); 
+    } 
+}
+
+
+
