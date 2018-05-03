103e7fe7d14da6415940aeccc31f51a9028e436d
ZFT <zhongfateng@126.com>
2018-05-03 14:31:35 +0800

    Merge branch 'master' of https://github.com/realdoctor/RealDoc
    # Conflicts:
    #	app/app.iml
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/real/doctor/realdoc/fragment/ProductShowFragment.java
@@ -0,0 +1,37 @@
+package com.real.doctor.realdoc.fragment;
+
+import android.content.Context;
+import android.view.View;
+
+import com.real.doctor.realdoc.R;
+import com.real.doctor.realdoc.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2018/4/18.
+ */
+
+public class ProductShowFragment extends BaseFragment {
+    public static ProductShowFragment newInstance() {
+        return new ProductShowFragment();
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_product_show;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+
+    @Override
+    public void widgetClick(View v) {
+
+    }
+}
