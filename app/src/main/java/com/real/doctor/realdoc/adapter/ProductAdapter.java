d4bb1ab69c2d98328277b42b65f60b4feca18496
ZFT <zhongfateng@126.com>
2018-05-03 17:30:58 +0800

    Merge branch 'master' of https://github.com/realdoctor/RealDoc
    # Conflicts:
    #	app/app.iml
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/real/doctor/realdoc/adapter/ProductAdapter.java
+++ app/src/main/java/com/real/doctor/realdoc/adapter/ProductAdapter.java
@@ -4,48 +4,44 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.real.doctor.realdoc.R;
 import com.real.doctor.realdoc.model.BrandBean;
+import com.real.doctor.realdoc.model.ProductInfo;
 
 import java.util.List;
 
 
-public class ProductAdapter extends RdBaseAdapter<BrandBean> {
-    private int selectedPosition = -1;
+public class ProductAdapter extends RdBaseAdapter<ProductInfo> {
     public ProductAdapter(Context context, List list) {
         super(context, list);
     }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        BrandBean bean = getItem(position);
+        ProductInfo bean = getItem(position);
         final BrandHolder holder;
         if (convertView == null) {
             holder = new BrandHolder();
-            convertView = mInflater.inflate(R.layout.brand_adapter_layout, parent, false);
-            holder.brandText = convertView.findViewById(R.id.tv_brand);
+            convertView = mInflater.inflate(R.layout.product_detail_item, parent, false);
+            holder.pTitle = (TextView) convertView.findViewById(R.id.product_detail_title);
+            holder.pContent=(TextView) convertView.findViewById(R.id.product_detail_content);
+            holder.pImage=(ImageView) convertView.findViewById(R.id.product_detail_img);
             convertView.setTag(holder);
         } else {
             holder = (BrandHolder) convertView.getTag();
         }
-        // 设置选中效果
-        if(selectedPosition == position)
-        {
-            holder.brandText.setTextColor(Color.BLACK);
-
-        } else {
-            holder.brandText.setTextColor(Color.parseColor("#494949"));
-        }
-        holder.brandText.setText(bean.breadName);
+        holder.pTitle.setText(bean.getName());
+        holder.pContent.setText(bean.getDesc());
+        Glide.with(mContext).load(bean.getImageUrl()).crossFade().into((ImageView) holder.pImage);
         return convertView;
     }
 
     public class BrandHolder {
-        private TextView brandText;
-    }
-    public void setSelectedPosition(int position) {
-        selectedPosition = position;
+        private ImageView pImage;
+        private TextView pTitle;
+        private TextView pContent;
     }
 }
